4b5b9475bd821d5bad4307c8578d07d8375e1314
Étienne Tétreault-Pinard <info@etpinard.xyz>
2021-02-17 10:22:21 -0500

    adapt usage.py

--- usage.py
+++ usage.py
@@ -1,23 +1,41 @@
 import dash_textarea_autocomplete
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_html_components as html
+
+WORD_LIST = ['apple', 'application', 'apartment',
+             'boat', 'banana', 'boss',
+             'coast', 'code', 'cat']
 
 app = dash.Dash(__name__)
 
 app.layout = html.Div([
     dash_textarea_autocomplete.DashTextareaAutocomplete(
         id='input',
-        value='my-value',
-        label='my-label'
+        # value='initial value',
+        placeholder='Type something, use `:` to invoke auto-completion',
+        wordList=WORD_LIST,
+        # common options with their default values
+        # triggerChar=':',
+        # minChar=1
     ),
-    html.Div(id='output')
+    html.Div(id='output'),
+    html.Button('CLICK', id='btn'),
+    html.Div(id='output2')
 ])
 
 
-@app.callback(Output('output', 'children'), [Input('input', 'value')])
+@app.callback(Output('output', 'children'),
+              Input('input', 'value'))
 def display_output(value):
-    return 'You have entered {}'.format(value)
+    return 'You have entered: {}'.format(value)
+
+
+@app.callback(Output('output2', 'children'),
+              Input('btn', 'n_clicks'),
+              State('input', 'value'))
+def display_output2(n_clicks, value):
+    return 'After click, you have: {}'.format(value)
 
 
 if __name__ == '__main__':
