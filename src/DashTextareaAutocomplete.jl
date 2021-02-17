e2b326d9413b73b503fa86e49af60c0b734d8ae7
Étienne Tétreault-Pinard <info@etpinard.xyz>
2021-02-17 15:22:20 -0500

    0.2.0

--- src/DashTextareaAutocomplete.jl
+++ src/DashTextareaAutocomplete.jl
@@ -3,7 +3,7 @@ module DashTextareaAutocomplete
 using Dash
 
 const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
-const version = "0.1.3"
+const version = "0.2.0"
 
 include("dashtextareaautocomplete.jl")
 
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js",
-    external_url = "https://unpkg.com/dash_textarea_autocomplete@0.1.3/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
+    external_url = "https://unpkg.com/dash_textarea_autocomplete@0.2.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js.map",
-    external_url = "https://unpkg.com/dash_textarea_autocomplete@0.1.3/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
+    external_url = "https://unpkg.com/dash_textarea_autocomplete@0.2.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
