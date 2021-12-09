4d8f0dad5be928a1bed74fb0c92dc1ff747d13c4
= <Felix.Gauthier@r2.ca>
2021-12-09 23:31:38 +0000

    Updated resources_path and external_url

--- src/DashTextareaAutocomplete.jl
+++ src/DashTextareaAutocomplete.jl
@@ -2,7 +2,7 @@
 module DashTextareaAutocomplete
 using Dash
 
-const resources_path = realpath(joinpath( @__DIR__, "..", "deps"))
+const resources_path = artifact"dash_textarea_autocomplete_resources"
 const version = "1.1.0"
 
 include("jl/dashtextareaautocomplete.jl")
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js",
-    external_url = "https://unpkg.com/dash_textarea_autocomplete@1.1.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
+    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.1.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js.map",
-    external_url = "https://unpkg.com/dash_textarea_autocomplete@1.1.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
+    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.1.0/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
