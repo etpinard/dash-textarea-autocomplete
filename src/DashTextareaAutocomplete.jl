1a69c434ab5985a4938ca6d0a5da284c8628c046
Étienne Tétreault-Pinard <info@etpinard.xyz>
2021-12-20 11:01:33 -0500

    1.3.2

--- src/DashTextareaAutocomplete.jl
+++ src/DashTextareaAutocomplete.jl
@@ -3,7 +3,7 @@ module DashTextareaAutocomplete
 using Dash, Pkg.Artifacts
 
 resources_path() = artifact"dash_textarea_autocomplete_resources"
-const version = "1.3.1"
+const version = "1.3.2"
 
 include("jl/dashtextareaautocomplete.jl")
 
@@ -16,14 +16,14 @@ function __init__()
             [
                 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js",
-    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.3.1/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
+    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.3.2/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js",
     dynamic = nothing,
     async = nothing,
     type = :js
 ),
 DashBase.Resource(
     relative_package_path = "dash_textarea_autocomplete.min.js.map",
-    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.3.1/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
+    external_url = "https://unpkg.com/dash-textarea-autocomplete@1.3.2/dash_textarea_autocomplete/dash_textarea_autocomplete.min.js.map",
     dynamic = true,
     async = nothing,
     type = :js
